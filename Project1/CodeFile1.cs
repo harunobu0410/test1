559e031d2ca7334088034ab9703a98ea19f79b26
harunobuichikawa <ichikawa-h@dainichi-consul.co.jp>
2016-04-08 16:40:48 +0900

    test2

--- Project1/CodeFile1.cs
+++ Project1/CodeFile1.cs
@@ -3,6 +3,8 @@
 class test {
     public static void Main() {
         int x = 10, y = 20;
+        int z = 30;
         Console.WriteLine("{0}{1}", x, y);
+        Console.WriteLine("{0}", z);
     }
 }
